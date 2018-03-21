e4ee793b5c6ac8bbcf150bd794dc5ae9435ecf8f
luciano ratamero <luciano@ratamero.com>
2018-03-21 04:16:44 -0300

    cleans runtests.py

--- runtests.py
+++ runtests.py
@@ -13,23 +13,3 @@ if __name__ == "__main__":
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
     sys.exit(bool(failures))
-#
-#
-#
-# import os
-# import sys
-# import django
-# from django.test.runner import DiscoverRunner
-#
-# sys.path.append(os.path.normpath(os.path.join(os.getcwd(), '../..')))
-# print(sys.path)
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-# django.setup()
-#
-#
-# import ipdb; ipdb.set_trace()
-# tr = DiscoverRunner(verbosity=1)
-# tr.setup_test_environment()
-# failures = tr.run_tests(['django_apistar', ])
-# if failures:
-#     sys.exit(failures)
