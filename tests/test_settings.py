17068886400ef94dfeb667d582bfc962af38205f
Luciano Ratamero <luciano@ratamero.com>
2018-03-24 00:25:45 -0300

    adds test module, with a client and a test case.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,12 +6,12 @@ SECRET_KEY = 'o=v^sgst0up0^znuce#97(6o5uwxi+nv!ur@b(6(js50808(%&'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'database.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
